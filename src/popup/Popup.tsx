895c5962ffee1a349f3194333ba8a456d464f85d
Abdelrahman Seada <abdom.seada@gmail.com>
2024-11-17 00:59:36 +0200

    removed tailwind

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,18 +1,18 @@
-// Popup.tsx
+
 import { usePopupStore } from './stores/popup.store';
 import PopupView from './components/PopupView';
-import ReactDOM from 'react-dom'; // Import ReactDOM for portals
+import ReactDOM from 'react-dom'; 
 
 export default function Popup() {
   const popups = usePopupStore(state => state.popups);
 
-  // Render all popups as a portal to `document.body`
+  
   return ReactDOM.createPortal(
     <>
       {popups.map((popup, index) => (
         <PopupView key={index} order={index} data={popup} />
       ))}
     </>,
-    document.body // Specify document.body as the portal target
+    document.body 
   );
 }
--- src/popup/components/PopupView.tsx
+++ src/popup/components/PopupView.tsx
@@ -1,67 +1,125 @@
-import { AiOutlineClose } from "react-icons/ai"; 
-import React, { useEffect } from "react"
-import  {  usePopupStore, IPopup } from "../stores/popup.store"
-import { cn } from "../../config/cn.helper"
+// import { AiOutlineClose } from "react-icons/ai"; 
+// import React, { useEffect } from "react"
+// import  {  usePopupStore, IPopup } from "../stores/popup.store"
+// import { cn } from "../../config/cn.helper"
 
+
+// type Props = {
+//   data : IPopup
+//   order : number
+// }
+
+
+// export default function PopupView(props : Props) {
+
+//   const {close , Element , id} = props.data
+
+//   const closeLatest = usePopupStore(state => state.closeLatest)
+
+//   const zIndex = 1000 + (props.order+1) * 2
+
+//   const onClose = () => {
+//     close(id)
+//   }
+
+//   useEffect(() => {
+//     const mouseDownHandler = (e : MouseEvent) => {
+//       const target = e.target as HTMLElement
+//       const isPopup = !!target.closest("[data-popup-preview]")
+
+//       if(isPopup) return
+      
+//       closeLatest()
+//     }
+//     window.addEventListener("mousedown" , mouseDownHandler)
+//     return () => {
+//       window.removeEventListener("mousedown" , mouseDownHandler)
+//     }
+//   }, [])
+
+//   return (
+//   <>
+  
+//   <div 
+//         data-popup-preview 
+//         style={{zIndex}}
+//         {...props.data.props} 
+//         className={cn(
+//         "bg-white shadow-lg  p-2  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ring-1 ring-[#efefef]/50",
+//         "min-w-[40vw] min-h-fit max-w-[90vw] max-h-[90vh]  w-fit",
+//         "rounded-md p-4 pt-3",
+//         props.data.props?.className
+//       )}>
+
+
+//       { React.cloneElement<{ item?: any , close : () => void}>( Element, { close : onClose  } )}
+
+//       <button onClick={onClose} className="absolute top-3 right-3 border-border px-2 py-1 rounded-md">
+//         <AiOutlineClose />
+//       </button>
+
+//     </div>
+
+//     <div className="w-full h-full backdrop-blur-[1px] absolute top-0 left-0 bg-black/50" style={{zIndex : zIndex - 1}}/>  
+  
+//   </>
+//   )
+// }
+import { AiOutlineClose } from "react-icons/ai";
+import React, { useEffect } from "react";
+import { usePopupStore, IPopup } from "../stores/popup.store";
+import "./styles.css"
 
 type Props = {
-  data : IPopup
-  order : number
-}
+  data: IPopup;
+  order: number;
+};
 
+export default function PopupView(props: Props) {
+  const { close, Element, id } = props.data;
 
-export default function PopupView(props : Props) {
+  const closeLatest = usePopupStore((state) => state.closeLatest);
 
-  const {close , Element , id} = props.data
-
-  const closeLatest = usePopupStore(state => state.closeLatest)
-
-  const zIndex = 1000 + (props.order+1) * 2
+  const zIndex = 1000 + (props.order + 1) * 2;
 
   const onClose = () => {
-    close(id)
-  }
+    close(id);
+  };
 
   useEffect(() => {
-    const mouseDownHandler = (e : MouseEvent) => {
-      const target = e.target as HTMLElement
-      const isPopup = !!target.closest("[data-popup-preview]")
+    const mouseDownHandler = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      const isPopup = !!target.closest("[data-popup-preview]");
 
-      if(isPopup) return
-      
-      closeLatest()
-    }
-    window.addEventListener("mousedown" , mouseDownHandler)
+      if (isPopup) return;
+
+      closeLatest();
+    };
+    window.addEventListener("mousedown", mouseDownHandler);
     return () => {
-      window.removeEventListener("mousedown" , mouseDownHandler)
-    }
-  }, [])
+      window.removeEventListener("mousedown", mouseDownHandler);
+    };
+  }, []);
 
   return (
-  <>
-  
-  <div 
-        data-popup-preview 
-        style={{zIndex}}
-        {...props.data.props} 
-        className={cn(
-        "bg-white shadow-lg  p-2  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ring-1 ring-[#efefef]/50",
-        "min-w-[40vw] min-h-fit max-w-[90vw] max-h-[90vh]  w-fit",
-        "rounded-md p-4 pt-3",
-        props.data.props?.className
-      )}>
+    <>
+      <div data-popup-preview
+        style={{ zIndex }}
+        {...props.data.props}
+        className={`_airpop-popup-container ${props.data.props?.className || ""}`}
+      >
+        {React.cloneElement<{ item?: any; close: () => void }>(Element, {
+          close: onClose,
+        })}
+        <button onClick={onClose} className="_airpop-popup-close-button">
+          <AiOutlineClose />
+        </button>
+      </div>
 
-
-      { React.cloneElement<{ item?: any , close : () => void}>( Element, { close : onClose  } )}
-
-      <button onClick={onClose} className="absolute top-3 right-3 border-border px-2 py-1 rounded-md">
-        <AiOutlineClose />
-      </button>
-
-    </div>
-
-    <div className="w-full h-full backdrop-blur-[1px] absolute top-0 left-0 bg-black/50" style={{zIndex : zIndex - 1}}/>  
-  
-  </>
-  )
+      <div
+        className="_airpop-popup-backdrop"
+        style={{ zIndex: zIndex - 1 }}
+      />
+    </>
+  );
 }
